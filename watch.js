a183b763d240ad774aa4d2f3ab76f94130198c08
ChanWahFung <552095989@qq.com>
2019-11-14 23:15:22 +0800

    [update] watch 添加对象配置

--- watch.js
+++ watch.js
@@ -1,45 +1,61 @@
-let watch = (function(){
-  function setWatch(ctx){
+let watch = (function () {
+  function setWatch(ctx) {
     let data = ctx.data,
-        watch = ctx.watch
-    if(!isType(watch,'object')){
+      watch = ctx.watch
+    if (!isType(watch, 'object')) {
       return
     }
-    Object.keys(watch).forEach(item=>{
+    Object.keys(watch).forEach(item => {
       //处理点语法的watch
-      let keys = item.split('.')
-      let targetData = data
-      let targetKey = keys[keys.length-1]
-      keys.length > 1 && keys.pop() && keys.forEach(key=>{targetData = targetData[key]})
+      let keys = item.split('.'),
+        targetData = data,
+        targetKey = keys[keys.length - 1],
+        watchFn = watch[item],
+        deepFlag = false;
+      keys.length > 1 && keys.pop() && keys.forEach(key => { targetData = targetData[key] })
+      //watch属性为对象的情况
+      if (isType(watchFn, 'object')) {
+        watchFn = watch[item].handler
+        deepFlag = watch[item].deep
+      }
       //监听
-      observe(targetData,targetKey,watch[item],ctx)
+      observe(targetData, targetKey, watchFn, deepFlag, ctx)
     })
   }
   //监听属性 Object.defineProperty
-  function observe(obj,key,watchFn,ctx){
-    let oldVal = obj[key]
-    if(isType(oldVal,'null') || isType(oldVal,'object')){
+  function observe(obj, key, watchFn, deepFlag, ctx) {
+    let cacheVal = obj[key]
+    if (isType(cacheVal, 'null') || isType(cacheVal, 'array')) {
       return
     }
-    Object.defineProperty(obj,key,{
-      configurable: true,
-      enumerable: true,
-      set(newVal){
-        // 处理不同类型的监听句柄
-        if(isType(watchFn,'array')){
-          watchFn.forEach(item=>{runWatchFn(item,ctx,newVal,oldVal)})
-        }else{
-          runWatchFn(watchFn,ctx,newVal,oldVal)
+    if (deepFlag && isType(cacheVal, 'object')) {
+      Object.keys(cacheVal).forEach(cacheValkey => {
+        observe(cacheVal, cacheValkey, watchFn, deepFlag, ctx)
+      })
+    }
+    if (!isType(cacheVal, 'object')) {
+      Object.defineProperty(obj, key, {
+        configurable: true,
+        enumerable: true,
+        set(newVal) {
+          if (newVal === cacheVal) return
+          // 处理不同类型的监听句柄
+          if (isType(watchFn, 'array')) {
+            watchFn.forEach(item => { runWatchFn(item, ctx, newVal, cacheVal) })
+          } else {
+            runWatchFn(watchFn, ctx, newVal, cacheVal)
+          }
+          cacheVal = newVal
         }
-      }
-    })
+      })
+    }
   }
   //执行watch句柄
-  function runWatchFn(watchFn,ctx,newVal,oldVal){
-    if(isType(watchFn,'string')){
+  function runWatchFn(watchFn, ctx, newVal, oldVal) {
+    if (isType(watchFn, 'string')) {
       watchFn = ctx[watchFn]
     }
-    isType(watchFn,'function') && watchFn.call(ctx,newVal,oldVal)
+    isType(watchFn, 'function') && watchFn.call(ctx, newVal, oldVal)
   }
   //类型工具函数
   function isType(target, type) {
@@ -47,6 +63,6 @@ let watch = (function(){
     type = type.toLowerCase()
     return targetType === type
   }
-  return {setWatch}
+  return { setWatch }
 })()
 export default watch
